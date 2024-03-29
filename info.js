05d685cde32ba2584695278bb060073d167e684d
annemauie <150228128+annemauie@users.noreply.github.com>
2024-03-29 15:44:35 +0800

    Update info.js

--- info.js
+++ info.js
@@ -3,7 +3,7 @@ const leftDiv = (title, para, button) => {
     mydiv.className = "leftClass"
     mydiv.innerHTML = `<h1 class="title1">${title}</h1>
                         <p>${para}</p>
-                        <button class="sec1btn">${button}</button>
+                        <button class="sec1btn"><a href="https://www.facebook.com/mediantequeenannelasdoce" class="contact">${button}</a></button>
                         `
     return mydiv
 }
